5624b20b4eaac95b9bd64d970285aa88a7e550fe
enrico-elizan <50596463+enrico-elizan@users.noreply.github.com>
2019-10-21 16:39:26 -0400

    Added connection string

--- BookApiProject/BookApiProject/Startup.cs
+++ BookApiProject/BookApiProject/Startup.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookApiProject
 {
     public class Startup
     {
+        // inject to use connetion string for db through IConfiguration
+        // allows to store string in app.json file
+        // then create constructor 
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration; 
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             // configure project to use MVC
-            services.AddMvc(); 
+            services.AddMvc();
+            // create connection string in JSON Format and will be used in appsettings.json
+            var connectionString = Configuration["connectionStrings:bookDbConnectionString"];
         }
 
         // Middleware, interfaces, database services will be registered here
